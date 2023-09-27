11b8b03cae44347c6105d5a9040b7a4f131e0567
safimiazi <mohibullamiazi@gmail.com>
2023-09-27 20:39:27 +0600

    home page update

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -2,14 +2,17 @@ import Chart from "../../Components/Chart/Chart";
 
 
 const Statistics = () => {
+
     return (
-        <div className="flex justify-center my-10">
+       <div>
+         <div className="flex justify-center my-10">
            <Chart></Chart> 
            <div>
             <p>Your Donation  <span className="mx- text-[#00C49F] rounded bg-[#00C49F]">......................</span></p>
             <p>Total Donation <span className=" text-[#f0644f] rounded bg-[#f0644f]">......................</span></p>
            </div>
         </div>
+       </div>
     );
 };
 
